8bd49f2332d79c6d54a0a730965d84cd765438fb
Mario Steele <eumario@refinerycms.subnet12092331.vcn12092331.oraclevcn.com>
2022-05-01 04:28:24 +0000

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Hangfire;
+using Hangfire.LiteDB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHangfire(configuration => 
+{
+    configuration.UseLiteDbStorage("./hf.db");
+});
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+} else {
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+
+
+app.Run();
+
+// record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
